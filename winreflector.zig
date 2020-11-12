b913717a25ba34614191bcd8821d92369a46b182
Jonathan Marler <johnnymarler@gmail.com>
2020-11-11 20:04:01 -0700

    fix newlines

--- winreflector.zig
+++ winreflector.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const windows = @cImport({
     @cDefine("_AMD64_", "");
     //@cDefine("_X86_", "");
-    
+
     //@cInclude("windef.h");
     @cInclude("windows.h");
 });
