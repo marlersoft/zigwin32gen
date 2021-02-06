565dc1beeac68e192c4fe9c478e5a4858e91fd0d
Jonathan Marler <johnnymarler@gmail.com>
2021-02-05 19:39:31 -0700

    prepare code for more type references

--- src/stringpool.zig
+++ src/stringpool.zig
@@ -7,26 +7,50 @@ const zog = @import("./zog.zig");
 /// Takes an allocator and manages a set of strings.
 /// Every string in the pool is owned by the pool.
 pub const StringPool = struct {
+    pub const Val = struct {
+        slice: []const u8,
+        pub fn format(
+            self: @This(),
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) std.os.WriteError!void {
+            return writer.writeAll(self.slice);
+        }
+    };
+
     allocator: *std.mem.Allocator,
-    map: StringHashMap([]const u8),
+    map: StringHashMap(Val),
     pub fn init(allocator: *std.mem.Allocator) @This() {
         return @This() {
             .allocator = allocator,
-            .map = StringHashMap([]const u8).init(allocator),
+            .map = StringHashMap(Val).init(allocator),
         };
     }
     /// If the pool already contains this a string that matches the contents
     /// of the given string, return the existing string from this pool.
     /// Otherwise, create a copy of this string, add it to the pool and return
     /// the new copy.
-    pub fn add(self: *@This(), s: []const u8) ![]const u8 {
+    pub fn add(self: *@This(), s: []const u8) !Val {
         if (self.map.get(s)) |entry| {
             return entry;
         }
         var newString = try self.allocator.alloc(u8, s.len);
         std.mem.copy(u8, newString, s);
-        _ = try self.map.put(newString, newString);
-        return newString;
+        const val = Val { .slice = newString };
+        _ = try self.map.put(newString, val);
+        return val;
+    }
+
+    fn eqlVal(a: Val, b: Val) bool {
+        return std.hash_map.eqlString(a.slice, b.slice);
+    }
+    fn hashVal(s: Val) u64 {
+        return std.hash_map.hashString(s.slice);
+    }
+
+    pub fn HashMap(comptime V: type) type {
+        return std.HashMap(Val, V, hashVal, eqlVal, std.hash_map.DefaultMaxLoadPercentage);
     }
 };
 
