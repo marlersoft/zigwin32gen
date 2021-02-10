100ddb1d79fd83da3b4a91d592823721c673e915
Jonathan Marler <johnnymarler@gmail.com>
2021-02-10 11:44:06 -0700

    finish porting opendialog.zig, not working yet though

--- examples/opendialog.zig
+++ examples/opendialog.zig
@@ -5,8 +5,6 @@ pub const UNICODE = true;
 const WINAPI = @import("std").os.windows.WINAPI;
 
 const win32 = @import("win32");
-//#include <windows.h>
-//#include <shobjidl.h> 
 usingnamespace win32.zig;
 usingnamespace win32.api.system_services;
 usingnamespace win32.api.windows_and_messaging;
@@ -28,32 +26,34 @@ pub export fn wWinMain(hInstance: HINSTANCE, _: HINSTANCE, pCmdLine: [*:0]u16, n
         hr = CoCreateInstance(CLSID_FileOpenDialog, null, @enumToInt(win32.missing.CLSCTX_ALL),
                 IID_IFileOpenDialog, @ptrCast(**c_void, &pFileOpen));
 
-//        if (SUCCEEDED(hr))
-//        {
-//            // Show the Open dialog box.
-//            hr = pFileOpen->Show(NULL);
-//
-//            // Get the file name from the dialog box.
-//            if (SUCCEEDED(hr))
-//            {
-//                IShellItem *pItem;
-//                hr = pFileOpen->GetResult(&pItem);
-//                if (SUCCEEDED(hr))
-//                {
-//                    PWSTR pszFilePath;
-//                    hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
-//
-//                    // Display the file name to the user.
-//                    if (SUCCEEDED(hr))
-//                    {
-//                        MessageBoxW(NULL, pszFilePath, L"File Path", MB_OK);
-//                        CoTaskMemFree(pszFilePath);
-//                    }
-//                    pItem->Release();
-//                }
-//            }
-//            pFileOpen->Release();
-//        }
+        if (SUCCEEDED(hr))
+        {
+            // Show the Open dialog box.
+            hr = pFileOpen.IModalWindow_Show(null);
+
+            // Get the file name from the dialog box.
+            if (SUCCEEDED(hr))
+            {
+                var pItem: *IShellItem = undefined;
+                hr = pFileOpen.IFileDialog_GetResult(&pItem);
+                if (SUCCEEDED(hr))
+                {
+                    var pszFilePath : [*:0]u16 = undefined;
+                    // NOTE: the second parameter of GetDisplayName is not properly typed
+                    //       this might be an issue with my codegen not handling multi-level pointer/array types
+                    hr = pItem.IShellItem_GetDisplayName(SIGDN_FILESYSPATH, @ptrCast(**u16, &pszFilePath));
+
+                    // Display the file name to the user.
+                    if (SUCCEEDED(hr))
+                    {
+                        _ = MessageBoxW(null, pszFilePath, L("File Path"), MB_OK);
+                        CoTaskMemFree(pszFilePath);
+                    }
+                    _ = pItem.IUnknown_Release();
+                }
+            }
+            _ = pFileOpen.IUnknown_Release();
+        }
         CoUninitialize();
     }
     return 0;
