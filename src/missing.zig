592278418c1058ddd162ee21f105ee975ad71c70
Jonathan Marler <johnnymarler@gmail.com>
2021-02-10 23:16:00 -0700

    add missing process access rights flags
    missing flags issue: https://github.com/microsoft/win32metadata/issues/217

--- src/missing.zig
+++ src/missing.zig
@@ -56,6 +56,25 @@ pub usingnamespace switch (@import("zig.zig").unicode_mode) {
     },
 };
 
+// The following flags seem to be missing, issue here: https://github.com/microsoft/win32metadata/issues/217
+pub const PROCESS_ALL_ACCESS = (
+    @enumToInt(win32.api.file_system.STANDARD_RIGHTS_REQUIRED) |
+    @enumToInt(win32.api.file_system.SYNCHRONIZE) |
+    0xFFF
+);
+pub const PROCESS_CREATE_PROCESS = 0x0080;
+pub const PROCESS_CREATE_THREAD = 0x0002;
+pub const PROCESS_DUP_HANDLE = 0x0040;
+pub const PROCESS_QUERY_INFORMATION = 0x0400;
+pub const PROCESS_QUERY_LIMITED_INFORMATION = 0x1000;
+pub const PROCESS_SET_INFORMATION = 0x0200;
+pub const PROCESS_SET_QUOTA = 0x0100;
+pub const PROCESS_SUSPEND_RESUME = 0x0800;
+pub const PROCESS_TERMINATE = 0x0001;
+pub const PROCESS_VM_OPERATION = 0x0008;
+pub const PROCESS_VM_READ = 0x0010;
+pub const PROCESS_VM_WRITE = 0x0020;
+
 const std = @import("std");
 test "" {
     std.testing.refAllDecls(@This());
