c48aad3608f5ef0a3f729157db76f39f3c731c6e
Jonathan Marler <johnnymarler@gmail.com>
2021-09-03 12:47:56 -0600

    fix examples

--- examples/net.zig
+++ examples/net.zig
@@ -8,8 +8,7 @@ usingnamespace win32.network_management.ip_helper;
 
 pub fn main() void {
     const s = socket(@enumToInt(AF_INET), SOCK_STREAM, @enumToInt(IPPROTO_TCP));
-    // workaround https://github.com/microsoft/win32metadata/issues/583
-    if (s == @intToPtr(SOCKET, INVALID_SOCKET)) {
+    if (s == INVALID_SOCKET) {
         std.log.err("socket failed with {}", .{GetLastError()});
     }
 }
