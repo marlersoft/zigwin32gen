42ae47964c4dd9185dc2f3c0e28c101d87db6f23
Watthanachai Dueanklang <bosskillerz@gmail.com>
2023-01-04 12:58:56 +0700

    Fixed: At least you can build it successfully. 	modified:   README.md 	modified:   examples/basewin.zig 	modified:   examples/helloworld-window.zig 	modified:   src/genzig.zig 	modified:   src/zig.zig

--- examples/basewin.zig
+++ examples/basewin.zig
@@ -18,73 +18,64 @@ const windowlongptr = @import("win32").windowlongptr;
 //       NOTE: should Zig allow symbol conflicts so long as they are not referenced?
 const mnr = @import("win32").ui.menus_and_resources;
 
-pub fn BaseWindow(comptime DERIVED_TYPE: type) type { return struct {
-
-    fn WindowProc(hwnd: HWND , uMsg: u32, wParam: win32.WPARAM, lParam: win32.LPARAM) callconv(WINAPI) win32.LRESULT
-    {
-        var pThis : ?*DERIVED_TYPE = null;
-        if (uMsg == win32.WM_NCCREATE)
-        {
-            const pCreate = @intToPtr(*win32.CREATESTRUCT, @bitCast(usize, lParam));
-            pThis = @ptrCast(*DERIVED_TYPE, @alignCast(@alignOf(DERIVED_TYPE), pCreate.lpCreateParams));
-            _ = windowlongptr.SetWindowLongPtr(hwnd, win32.GWL_USERDATA, @bitCast(isize, @ptrToInt(pThis)));
-            pThis.?.base.m_hwnd = hwnd;
+pub fn BaseWindow(comptime DERIVED_TYPE: type) type {
+    return struct {
+        fn WindowProc(hwnd: HWND, uMsg: u32, wParam: win32.WPARAM, lParam: win32.LPARAM) callconv(WINAPI) win32.LRESULT {
+            var pThis: ?*DERIVED_TYPE = null;
+            if (uMsg == win32.WM_NCCREATE) {
+                const pCreate = @intToPtr(*win32.CREATESTRUCT, @bitCast(usize, lParam));
+                pThis = @ptrCast(*DERIVED_TYPE, @alignCast(@alignOf(DERIVED_TYPE), pCreate.lpCreateParams));
+                _ = windowlongptr.SetWindowLongPtr(hwnd, win32.GWL_USERDATA, @bitCast(isize, @ptrToInt(pThis)));
+                pThis.?.base.m_hwnd = hwnd;
+            } else {
+                pThis = @intToPtr(?*DERIVED_TYPE, @bitCast(usize, windowlongptr.GetWindowLongPtr(hwnd, win32.GWL_USERDATA)));
+            }
+            if (pThis) |this| {
+                return this.HandleMessage(uMsg, wParam, lParam);
+            } else {
+                return win32.DefWindowProc(hwnd, uMsg, wParam, lParam);
+            }
         }
-        else
-        {
-            pThis = @intToPtr(?*DERIVED_TYPE, @bitCast(usize, windowlongptr.GetWindowLongPtr(hwnd, win32.GWL_USERDATA)));
+
+        pub fn Create(
+            self: *@This(),
+            lpWindowName: [*:0]const u16,
+            dwStyle: win32.WINDOW_STYLE,
+            options: struct {
+                dwExStyle: win32.WINDOW_EX_STYLE = @intToEnum(win32.WINDOW_EX_STYLE, 0),
+                x: i32 = win32.CW_USEDEFAULT,
+                y: i32 = win32.CW_USEDEFAULT,
+                nWidth: i32 = win32.CW_USEDEFAULT,
+                nHeight: i32 = win32.CW_USEDEFAULT,
+                hWndParent: ?HWND = null,
+                hMenu: ?win32.HMENU = null,
+            },
+        ) win32.BOOL {
+            const wc = win32.WNDCLASS{
+                .style = @intToEnum(win32.WNDCLASS_STYLES, 0),
+                .lpfnWndProc = WindowProc,
+                .cbClsExtra = 0,
+                .cbWndExtra = 0,
+                .hInstance = win32.GetModuleHandle(null),
+                .hIcon = null,
+                .hCursor = win32.LoadCursor(null, win32.IDC_ARROW),
+                .hbrBackground = null,
+                // TODO: autogen bindings don't allow for null, should win32metadata allow Option for fields? Or should all strings allow NULL?
+                .lpszMenuName = L("Placeholder"),
+                .lpszClassName = DERIVED_TYPE.ClassName(),
+            };
+
+            _ = win32.RegisterClass(&wc);
+
+            self.m_hwnd = win32.CreateWindowEx(options.dwExStyle, DERIVED_TYPE.ClassName(), lpWindowName, dwStyle, options.x, options.y, options.nWidth, options.nHeight, options.hWndParent, options.hMenu, win32.GetModuleHandle(null), @ptrCast(*anyopaque, self));
+
+            return if (self.m_hwnd != null) win32.TRUE else win32.FALSE;
         }
-        if (pThis) |this|
-        {
-            return this.HandleMessage(uMsg, wParam, lParam);
+
+        pub fn Window(self: @This()) ?HWND {
+            return self.m_hwnd;
         }
-        else
-        {
-            return win32.DefWindowProc(hwnd, uMsg, wParam, lParam);
-        }
-    }
 
-    pub fn Create(self: *@This(),
-        lpWindowName: [*:0]const u16,
-        dwStyle: win32.WINDOW_STYLE,
-        options: struct {
-            dwExStyle: win32.WINDOW_EX_STYLE = @intToEnum(win32.WINDOW_EX_STYLE, 0),
-            x: i32 = win32.CW_USEDEFAULT,
-            y: i32 = win32.CW_USEDEFAULT,
-            nWidth: i32 = win32.CW_USEDEFAULT,
-            nHeight: i32 = win32.CW_USEDEFAULT,
-            hWndParent: ?HWND = null,
-            hMenu: ?win32.HMENU = null,
-        },
-    ) win32.BOOL {
-        const wc = win32.WNDCLASS {
-            .style = @intToEnum(win32.WNDCLASS_STYLES, 0),
-            .lpfnWndProc = WindowProc,
-            .cbClsExtra = 0,
-            .cbWndExtra = 0,
-            .hInstance = win32.GetModuleHandle(null),
-            .hIcon = null,
-            .hCursor = null,
-            .hbrBackground = null,
-            // TODO: autogen bindings don't allow for null, should win32metadata allow Option for fields? Or should all strings allow NULL?
-            .lpszMenuName = L("Placeholder"),
-            .lpszClassName = DERIVED_TYPE.ClassName(),
-        };
-
-        _ = win32.RegisterClass(&wc);
-
-        self.m_hwnd = win32.CreateWindowEx(
-            options.dwExStyle, DERIVED_TYPE.ClassName(), lpWindowName,
-            dwStyle, options.x, options.y,
-            options.nWidth, options.nHeight, options.hWndParent, options.hMenu,
-            win32.GetModuleHandle(null),
-            @ptrCast(*anyopaque, self)
-            );
-
-        return if (self.m_hwnd != null) win32.TRUE else win32.FALSE;
-    }
-
-    pub fn Window(self: @This()) ?HWND { return self.m_hwnd; }
-
-    m_hwnd: ?HWND = null,
-};}
+        m_hwnd: ?HWND = null,
+    };
+}
--- examples/helloworld-window.zig
+++ examples/helloworld-window.zig
@@ -16,21 +16,20 @@ const CW_USEDEFAULT = win32.CW_USEDEFAULT;
 const MSG = win32.MSG;
 const HWND = win32.HWND;
 
-pub export fn wWinMain(hInstance: HINSTANCE, _: ?HINSTANCE, pCmdLine: [*:0]u16, nCmdShow: u32) callconv(WINAPI) c_int
-{
+pub export fn wWinMain(hInstance: HINSTANCE, _: ?HINSTANCE, pCmdLine: [*:0]u16, nCmdShow: u32) callconv(WINAPI) c_int {
     _ = pCmdLine;
 
     // Register the window class.
     const CLASS_NAME = L("Sample Window Class");
 
-    const wc = win32.WNDCLASS {
+    const wc = win32.WNDCLASS{
         .style = @intToEnum(win32.WNDCLASS_STYLES, 0),
         .lpfnWndProc = WindowProc,
         .cbClsExtra = 0,
         .cbWndExtra = 0,
         .hInstance = hInstance,
         .hIcon = null,
-        .hCursor = null,
+        .hCursor = win32.LoadCursor(null, win32.IDC_ARROW),
         .hbrBackground = null,
         // TODO: this field is not marked as options so we can't use null atm
         .lpszMenuName = L("Some Menu Name"),
@@ -41,31 +40,26 @@ pub export fn wWinMain(hInstance: HINSTANCE, _: ?HINSTANCE, pCmdLine: [*:0]u16, 
 
     // Create the window.
 
-    const hwnd = win32.CreateWindowEx(
-        @intToEnum(win32.WINDOW_EX_STYLE, 0), // Optional window styles.
-        CLASS_NAME,                     // Window class
-        L("Learn to Program Windows"),  // Window text
-        win32.WS_OVERLAPPEDWINDOW,      // Window style
+    const hwnd = win32.CreateWindowEx(@intToEnum(win32.WINDOW_EX_STYLE, 0), // Optional window styles.
+        CLASS_NAME, // Window class
+        L("Learn to Program Windows"), // Window text
+        win32.WS_OVERLAPPEDWINDOW, // Window style
 
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-        null,       // Parent window
-        null,       // Menu
-        hInstance,  // Instance handle
-        null        // Additional application data
+    // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, null, // Parent window
+        null, // Menu
+        hInstance, // Instance handle
+        null // Additional application data
     );
-    if (hwnd == null)
-    {
+    if (hwnd == null) {
         return 0;
     }
 
     _ = win32.ShowWindow(hwnd, @intToEnum(win32.SHOW_WINDOW_CMD, nCmdShow));
 
     // Run the message loop.
-    var msg : MSG = undefined;
-    while (win32.GetMessage(&msg, null, 0, 0) != 0)
-    {
+    var msg: MSG = undefined;
+    while (win32.GetMessage(&msg, null, 0, 0) != 0) {
         _ = win32.TranslateMessage(&msg);
         _ = win32.DispatchMessage(&msg);
     }
@@ -73,22 +67,18 @@ pub export fn wWinMain(hInstance: HINSTANCE, _: ?HINSTANCE, pCmdLine: [*:0]u16, 
     return 0;
 }
 
-fn WindowProc(hwnd: HWND , uMsg: u32, wParam: win32.WPARAM, lParam: win32.LPARAM) callconv(WINAPI) win32.LRESULT
-{
-    switch (uMsg)
-    {
-        win32.WM_DESTROY =>
-        {
+fn WindowProc(hwnd: HWND, uMsg: u32, wParam: win32.WPARAM, lParam: win32.LPARAM) callconv(WINAPI) win32.LRESULT {
+    switch (uMsg) {
+        win32.WM_DESTROY => {
             win32.PostQuitMessage(0);
             return 0;
         },
-        win32.WM_PAINT =>
-        {
+        win32.WM_PAINT => {
             var ps: win32.PAINTSTRUCT = undefined;
             const hdc = win32.BeginPaint(hwnd, &ps);
 
             // All painting occurs here, between BeginPaint and EndPaint.
-            _ = win32.FillRect(hdc, &ps.rcPaint, @intToPtr(win32.HBRUSH, @as(usize, @enumToInt(win32.COLOR_WINDOW)+1)));
+            _ = win32.FillRect(hdc, &ps.rcPaint, @intToPtr(win32.HBRUSH, @as(usize, @enumToInt(win32.COLOR_WINDOW) + 1)));
             _ = win32.EndPaint(hwnd, &ps);
             return 0;
         },
