f98efe7dead6f2f1b3d4675caa002353efbceace
Wesley Koerber <wdkoerber@gmail.com>
2024-05-16 23:03:24 -0400

    chore: update with feedback

--- src/zigwin32.build.zig
+++ src/zigwin32.build.zig
@@ -1,10 +1,8 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
-const zig_version = builtin.zig_version;
-
 pub fn build(b: *std.Build) void {
-    switch (zig_version.minor) {
+    switch (builtin.zig_version.minor) {
         11 => {
             _ = b.addModule("zigwin32", .{
                 .source_file = .{ .path = "win32.zig" },
@@ -15,13 +13,10 @@ pub fn build(b: *std.Build) void {
                 .root_source_file = .{ .path = "win32.zig" },
             });
         },
-        13 => {
+        else => {
             _ = b.addModule("zigwin32", .{
                 .root_source_file = b.path("win32.zig"),
             });
-        },
-        else => {
-            @panic("Unsupported zig version '" ++ builtin.zig_version_string ++ "'");
         },
     }
 }
